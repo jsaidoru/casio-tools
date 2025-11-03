cba3da7f2f391fba5293b9554e8e1ad3f594709e
John Ridge <johnridgevn@gmail.com>
2025-11-03 18:39:12 +0700

    init repo and added some stuff

---
+++ cogs/580vnx.py
@@ -0,0 +1,17 @@
+from discord.ext import commands
+from pathlib import Path
+
+RESOURCE_PATH = Path(__file__).resolve().parent.parent / "resources" / "580vnx"
+font_1byte = RESOURCE_PATH / "display_font_1byte.png"
+font_2byte = RESOURCE_PATH / "display_font_2byte.png"
+
+class fx_580VNX(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name='display_font', help='Mở file bảng chữ 1 byte và 2 byte.')
+    async def display_font(self, ctx):
+        await ctx.send(file=[discord.File(fp=font_1byte), discord.File(fp=font_2byte)])
+
+async def setup(bot):
+    await bot.add_cog(fx_580VNX(bot))
---
+++ main.py
@@ -0,0 +1,17 @@
+import discord
+import os
+
+from discord.ext import commands
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="c!", intents=intents)
+@bot.event
+async def on_ready():
+    print(f'Bot is ready. Logged in as {bot.user}')
+    try:
+        await bot.load_extension('cogs.580vnx')
+    except Exception as e:
+        print(f"Error loading cogs or syncing commands: {e}")
+
+token = os.environ.get('BOT_TOKEN')
